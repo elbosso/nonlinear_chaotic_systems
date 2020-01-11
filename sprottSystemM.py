034b58fe720ce64de991c79e35fb7dd47b29838d
Juergen Key <sgjfsf@hotmail.com>
2020-01-11 17:26:16 +0000

    Sprott system M added

---
+++ sprottSystemM.py
@@ -0,0 +1,46 @@
+from ipywidgets import interact, interactive, HBox, Layout,VBox
+import numpy as np
+from scipy import integrate
+
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.colors import cnames
+from matplotlib import animation
+
+def solve_sprottm(numberOfTrajectories=10, anglex=0.0, angley=30.0, max_time=4.0):
+
+    fig = plt.figure(figsize=(8, 6), dpi=80)
+    ax = fig.add_axes([0, 0, 1, 1], projection='3d')
+    ax.axis('off')
+
+    # prepare the axes limits
+    ax.set_xlim((-40, 40))
+    ax.set_ylim((-60, 60))
+    ax.set_zlim((-555, 55))
+    
+    def sprottm_deriv(x_y_z, t0):
+        """Compute the time-derivative of a Sprott system M."""
+        x, y, z = x_y_z
+        return [-z, -x*x - y, 1.7 + 1.7*x + y]
+
+    # Choose random starting points, uniformly distributed from -15 to 15
+    np.random.seed(1)
+    x0 = -15 + 30 * np.random.random((numberOfTrajectories, 3))
+
+    # Solve for the trajectories
+    t = np.linspace(0, max_time, int(250*max_time))
+    x_t = np.asarray([integrate.odeint(sprottm_deriv, x0i, t)
+                      for x0i in x0])
+    
+    # choose a different color for each trajectory
+    colors = plt.cm.viridis(np.linspace(0, 1, numberOfTrajectories))
+
+    for i in range(numberOfTrajectories):
+        x, y, z = x_t[i,:,:].T
+        lines = ax.plot(x, y, z, '-', c=colors[i])
+        plt.setp(lines, linewidth=1)
+
+    ax.view_init(angley, anglex)
+    plt.show()
+
+    return t, x_t
