94c627da9e524914dc5001178a94513e741765ab
Jürgen Key <sgjfsf@hotmail.com>
2020-02-06 12:36:09 +0000

    re elbosso/website#73

---
+++ chuaSystem.py
@@ -0,0 +1,57 @@
+from ipywidgets import interact, interactive, HBox, Layout,VBox
+
+import numpy as np
+from scipy import integrate
+
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.colors import cnames
+from matplotlib import animation
+
+def solve_chua(numberOfTrajectories=10, anglex=0.0, angley=30.0, max_time=14.0, alpha=15.6, beta=1, gamma=28,m0=-1.143, m1=-0.714):
+
+    fig = plt.figure(figsize=(8, 6), dpi=80)
+    ax = fig.add_axes([0, 0, 1, 1], projection='3d')
+    ax.axis('off')
+
+    # prepare the axes limits
+    ax.set_xlim((-40, 40))
+    ax.set_ylim((-60, 60))
+    ax.set_zlim((5, 55))
+    
+    def g(x,m0,m1):
+        rv=0
+        if x<= -1:
+            rv=m1*x+m1-m0;
+        elif x<=1:
+            rv=m0*x;
+        else:
+            rv=m1*x+m0-m1
+        return rv
+    
+    def chua_deriv(x_y_z, t0, alpha=alpha, beta=beta, gamma=gamma,m0=m0,m1=m1):
+        """Compute the time-derivative of a Chua system."""
+        x, y, z = x_y_z
+        return [alpha * (y - x -g(x,m0,m1)), beta*(x - y + z), -gamma * y]
+
+    # Choose random starting points, uniformly distributed from -15 to 15
+    np.random.seed(1)
+    x0 = -15 + 30 * np.random.random((numberOfTrajectories, 3))
+
+    # Solve for the trajectories
+    t = np.linspace(0, max_time, int(250*max_time))
+    x_t = np.asarray([integrate.odeint(chua_deriv, x0i, t)
+                      for x0i in x0])
+    
+    # choose a different color for each trajectory
+    colors = plt.cm.viridis(np.linspace(0, 1, numberOfTrajectories))
+
+    for i in range(numberOfTrajectories):
+        x, y, z = x_t[i,:,:].T
+        lines = ax.plot(x, y, z, '-', c=colors[i])
+        plt.setp(lines, linewidth=1)
+
+    ax.view_init(angley, anglex)
+    plt.show()
+
+    return t, x_t
